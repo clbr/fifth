90415b9289b78c34e1e1d4b5008eb9c0627d3cfc
Lauri Kasanen <curaga@operamail.com>
2014-06-02 15:48:41 +0300

    Saving, start of loading

--- src/config.cpp
+++ src/config.cpp
@@ -16,12 +16,105 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "main.h"
 
+#define CONFIGFILE "config"
+
+static void parseLine(const char * const line) {
+
+}
+
+static void parseFile(FILE * const f) {
+
+}
+
 void loadConfig() {
 
 	// Setup defaults
 	g->settings = (setting *) xcalloc(1, sizeof(setting) * numDefaults);
+	memcpy(g->settings, defaultSettings, sizeof(setting) * numDefaults);
+
+	int fd = openat(g->profilefd, CONFIGFILE, O_RDONLY);
+	if (fd < 0) {
+		saveConfig();
+		return;
+	}
+
+	FILE *f = fdopen(fd, "r");
+	if (!f)
+		die("fdopen\n");
+
+	parseFile(f);
+
+	fclose(f);
 }
 
 void saveConfig() {
 
+	int fd = openat(g->profilefd, CONFIGFILE, O_WRONLY | O_TRUNC);
+	if (fd < 0)
+		die(_("Cannot open config file for saving\n"));
+
+	FILE *f = fdopen(fd, "w");
+	if (!f)
+		die("fdopen\n");
+
+	u32 i;
+	for (i = 0; i < numDefaults; i++) {
+		char desc;
+		switch (g->settings[i].type) {
+			case ST_CHAR:
+				desc = 'c';
+				fprintf(f, "%s %c %s\n", g->settings[i].name, desc,
+					g->settings[i].val.c);
+			break;
+			case ST_FLOAT:
+				desc = 'f';
+				fprintf(f, "%s %c %f\n", g->settings[i].name, desc,
+					g->settings[i].val.f);
+			break;
+			case ST_U32:
+				desc = 'u';
+				fprintf(f, "%s %c %u\n", g->settings[i].name, desc,
+					g->settings[i].val.u);
+			break;
+			case ST_COUNT:
+				die(_("Tried to save corrupt config\n"));
+		}
+	}
+
+	// Per-site config
+	map<string, vector<setting> >::const_iterator it;
+	for (it = g->sitesettings.begin(); it != g->sitesettings.end(); it++) {
+		const string &site = it->first;
+		const vector<setting> &set = it->second;
+
+		const u32 max = set.size();
+
+		for (i = 0; i < max; i++) {
+			char desc;
+			switch (set[i].type) {
+				case ST_CHAR:
+					desc = 'c';
+					fprintf(f, "site %s %s %c %s\n", site.c_str(),
+						set[i].name, desc,
+						set[i].val.c);
+				break;
+				case ST_FLOAT:
+					desc = 'f';
+					fprintf(f, "site %s %s %c %f\n", site.c_str(),
+						set[i].name, desc,
+						set[i].val.f);
+				break;
+				case ST_U32:
+					desc = 'u';
+					fprintf(f, "site %s %s %c %u\n", site.c_str(),
+						set[i].name, desc,
+						set[i].val.u);
+				break;
+				case ST_COUNT:
+					die(_("Tried to save corrupt config\n"));
+			}
+		}
+	}
+
+	fclose(f);
 }

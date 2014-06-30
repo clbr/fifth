25de06f5e2979ad5e8d3a85f9ba6815e6d9c7237
Lauri Kasanen <curaga@operamail.com>
2014-06-30 18:34:36 +0300

    Include fl_draw

--- src/main.h
+++ src/main.h
@@ -29,6 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <pthread.h>
 #include <locale.h>
 #include <FL/Fl.H>
+#include <FL/fl_draw.H>
 #include <webkit.h>
 
 #include "autoconfig.h"

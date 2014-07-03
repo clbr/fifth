ab48ba77560851387ed8485baaadd24318d83e26
Lauri Kasanen <curaga@operamail.com>
2014-07-03 18:29:36 +0300

    Align defaults table

--- src/defaults.c
+++ src/defaults.c
@@ -18,22 +18,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Must be in alphabetical order.
 const struct setting defaultSettings[] = {
-	{"fltk.scheme", ST_CHAR, { .c = "gtk+" }},
+	{"fltk.scheme",		ST_CHAR, { .c = "gtk+" }},
 	{"general.download-dir", ST_CHAR, { .c = "/tmp" }},
-	{"general.homepage", ST_CHAR, { .c = "about:blank"}},
-	{"general.javascript", ST_U32, { .u = 1}},
-	{"general.startup", ST_U32, { .u = 0}},
-	{"keys.back", ST_U32, { .u = 122}}, // z
-	{"keys.closetab", ST_U32, { .u = 33554551}}, // ctrl + w
-	{"keys.fwd", ST_U32, { .u = 120}}, // x
-	{"keys.newtab", ST_U32, { .u = 33554548}}, // ctrl + t
-	{"keys.quit", ST_U32, { .u = 33554545}}, // ctrl + q
-	{"keys.screencap", ST_U32, { .u = 33554547 }}, // ctrl + s
-	{"window.bars", ST_CHAR, { .c = "tab,url,main"}},
-	{"window.h", ST_U32, { .u = 600}},
-	{"window.w", ST_U32, { .u = 800}},
-	{"window.x", ST_U32, { .u = 0}},
-	{"window.y", ST_U32, { .u = 0}},
+	{"general.homepage", 	ST_CHAR, { .c = "about:blank"}},
+	{"general.javascript", 	ST_U32, { .u = 1}},
+	{"general.startup", 	ST_U32, { .u = 0}},
+	{"keys.back", 		ST_U32, { .u = 122}}, // z
+	{"keys.closetab", 	ST_U32, { .u = 33554551}}, // ctrl + w
+	{"keys.fwd", 		ST_U32, { .u = 120}}, // x
+	{"keys.newtab",	 	ST_U32, { .u = 33554548}}, // ctrl + t
+	{"keys.quit", 		ST_U32, { .u = 33554545}}, // ctrl + q
+	{"keys.screencap",	ST_U32, { .u = 33554547 }}, // ctrl + s
+	{"window.bars", 	ST_CHAR, { .c = "tab,url,main"}},
+	{"window.h", 		ST_U32, { .u = 600}},
+	{"window.w", 		ST_U32, { .u = 800}},
+	{"window.x", 		ST_U32, { .u = 0}},
+	{"window.y", 		ST_U32, { .u = 0}},
 };
 
 const u32 numDefaults = sizeof(defaultSettings) / sizeof(struct setting);

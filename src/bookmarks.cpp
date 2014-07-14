0e7891a859aeef1f604fc8daf57dfd3ee34b9695
Lauri Kasanen <curaga@operamail.com>
2014-07-14 14:02:39 +0300

    Bookmark loading, save placeholder

--- src/bookmarks.cpp
+++ src/bookmarks.cpp
@@ -16,10 +16,64 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "main.h"
 
+#define BOOKMARKFILE "bookmarks"
+
 void loadbookmarks() {
 
+	const int fd = openat(g->profilefd, BOOKMARKFILE, O_RDONLY);
+	if (fd < 0)
+		return;
+	FILE * const f = fdopen(fd, "r");
+	if (!f)
+		return;
+
+	enum {
+		BUFSIZE = 640
+	};
+
+	char tmp[BUFSIZE];
+	bookmark mark = { NULL, NULL };
+	while (fgets(tmp, BUFSIZE, f)) {
+		nukenewline(tmp);
+
+		if (strlen(tmp) < 2)
+			continue;
+
+		if (!strncmp(tmp, "name", 4)) {
+			if (mark.name || mark.url)
+				die("Corrupted bookmark file, line '%s'\n", tmp);
+
+			char *ptr = tmp + 5;
+			mark.name = NULL;
+			if (strlen(ptr) > 1)
+				mark.name = strdup(ptr);
+		} else if (!strncmp(tmp, "url", 3)) {
+			char *ptr = tmp + 4;
+			mark.url = NULL;
+			if (strlen(ptr) > 1)
+				mark.url = strdup(ptr);
+
+			g->bookmarks.push_back(mark);
+
+			mark.name = NULL;
+			mark.url = NULL;
+		} else {
+			die("Unknown bookmark line '%s'\n", tmp);
+		}
+	}
+
+	fclose(f);
 }
 
 void savebookmarks() {
+
+	const int fd = openat(g->profilefd, BOOKMARKFILE, O_WRONLY);
+	if (fd < 0)
+		die("Failed saving bookmarks\n");
+	FILE * const f = fdopen(fd, "w");
+	if (!f)
+		die("Failed saving bookmarks\n");
+
+	fclose(f);
 
 }

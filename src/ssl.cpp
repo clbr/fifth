144ba54e36cdeb4c8d6994da74bbf95aca336a99
Lauri Kasanen <curaga@operamail.com>
2014-07-18 13:40:58 +0300

    Implement SSL checks

--- src/ssl.cpp
+++ src/ssl.cpp
@@ -18,6 +18,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 int certcheck(const char *str, const char *host) {
 
-	printf("Checking '%s' for host '%s'\n", str, host);
+	char site[128];
+	url2site(host, site, 128);
+
+	const u32 len = strlen(str);
+	int fd = openat(g->certfd, site, O_RDONLY);
+	if (fd >= 0) {
+		// It must match byte-to-byte with the new one.
+		char buf[len];
+		if (read(fd, buf, len) != len)
+			die(_("Failed reading the certificate\n"));
+		close(fd);
+
+		if (memcmp(buf, str, len)) {
+			// TODO big scary SSL warning
+			return 0;
+		}
+	} else {
+		// Never seen this site before, save its cert
+		// TODO option for asking the user on all new certs
+		fd = openat(g->certfd, site, O_WRONLY | O_CREAT);
+		if (fd < 0)
+			die(_("Failed to create the certificate file\n"));
+
+		if (write(fd, str, len) != len)
+			die(_("Failed writing the certificate\n"));
+		close(fd);
+	}
+
 	return 1;
 }

5ef27e62327db21f3236d098ef1d32bfe463dbdd
Lauri Kasanen <curaga@operamail.com>
2014-07-22 16:54:14 +0300

    Support background tab opening

--- src/main.cpp
+++ src/main.cpp
@@ -400,6 +400,7 @@ int main(int argc, char **argv) {
 	wk_set_ssl_func(certcheck);
 	wk_set_ssl_err_func(certerr);
 	wk_set_popup_func(popupcb);
+	wk_set_bgtab_func(newtabbg);
 	wk_set_urlblock_func(isblocked);
 	wk_set_download_refresh_func(downloadrefresh);
 	wk_set_new_download_func(transfers);

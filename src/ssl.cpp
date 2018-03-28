3d608f78b318de4f5df80768a6bff1282b7013ed
Lauri Kasanen <cand@gmx.com>
2018-03-28 21:54:04 +0300

    Hard-whitelist google certs

--- src/ssl.cpp
+++ src/ssl.cpp
@@ -16,10 +16,58 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "main.h"
 
+static u8 domaintailmatch(const char * const str, const char * const end) {
+
+	const u32 len = strlen(str);
+	const u32 endlen = strlen(end);
+
+	if (endlen > len)
+		return 0;
+
+	if (len > endlen &&
+		str[len - endlen - 1] != '.' &&
+		str[len - endlen - 1] != '/')
+		return 0;
+
+	return strcmp(&str[len - endlen], end) == 0;
+}
+
+/* Google actively works against security by changing their certs
+   several times a week. Since having the user click through is
+   worthless, whitelist google sites. */
+static const char * const whited[] = {
+	"google.fi",
+	"google.com",
+	"googlecode.com",
+	"googledrive.com",
+	"googlegroups.com",
+	"googleusercontent.com",
+	"googlevideo.com",
+	"googleapis.com",
+	"goo.gl",
+	"gstatic.com",
+	"youtube.com",
+	"ytimg.com",
+};
+
+static u8 skipcert(const char *host) {
+
+	u32 i;
+	for (i = 0; i < sizeof(whited) / sizeof(whited[0]); i++) {
+		if (domaintailmatch(host, whited[i]))
+			return 1;
+	}
+
+	return 0;
+}
+
 int certcheck(const char *str, const char *host) {
 
 	char site[128];
 	url2site(host, site, 128, false);
+
+	if (skipcert(site))
+		return 1;
 
 	const u32 len = strlen(str);
 	int fd = openat(g->certfd, site, O_RDONLY);
